e7987ccfea7dd1114cff78d1aa5c97a39fb4526d
Munteanu Victor <munteanuvictor46@gmail.com>
2021-09-20 19:41:41 +0300

    added the "create" method

--- src/main/java/com/example/springbootex1/controller/CitiesController.java
+++ src/main/java/com/example/springbootex1/controller/CitiesController.java
@@ -1,4 +1,16 @@
 package com.example.springbootex1.controller;
 
+import com.example.springbootex1.repository.CitiesRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CitiesController {
+    CitiesRepository citiesRepository = new CitiesRepository();
+
+    @PostMapping("")
+    public String create(@RequestParam String citiesName){
+        return citiesRepository.create(citiesName);
+    }
 }
--- src/main/java/com/example/springbootex1/repository/CitiesRepository.java
+++ src/main/java/com/example/springbootex1/repository/CitiesRepository.java
@@ -1,4 +1,19 @@
 package com.example.springbootex1.repository;
 
+import java.util.ArrayList;
+
 public class CitiesRepository {
+    ArrayList<String> cities = new ArrayList<>();
+
+    public CitiesRepository(){
+        this.cities.add("Balti");
+        this.cities.add("Chisinau");
+        this.cities.add("Soroca");
+        this.cities.add("Tiraspol");
+    }
+
+    public String create(String citiesName){
+        this.cities.add(citiesName);
+        return "Create Cities";
+    }
 }
